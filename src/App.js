46027de71fd447e7d2b141a6235449def6e5e8e1
oleg-serf <olegserfcompany@gmail.com>
2021-02-19 11:22:51 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
   const [waterHeight, setWaterHeight] = useState(0);
-  
-  const [incTimer, setIncTimer] = useState(null);
-  const [decTimer, setDecTimer] = useState(null);
+  const [step, setStep] = useState(0);
 
-  const incFunc = () => {
-    setWaterHeight(height => height < 100 ? height+1 : height);
-  }
-  const decFunc = () => {
-    setWaterHeight(height => height > 0 ? height-1: height);
-  }
-
-  const onIncrease = () => {    
-    setTimeout(function (){
-      if(decTimer || waterHeight === 0) window.clearInterval(decTimer);
-      if(incTimer || waterHeight === 100) window.clearInterval(incTimer);
-    }, 0)
-
-    setTimeout(function (){
-      setIncTimer(setInterval(incFunc, 100));
-    }, 100)    
-  }
-
-  const onDecrease = () => {
-    setTimeout(function (){
-      if(decTimer || waterHeight === 0) window.clearInterval(decTimer);
-      if(incTimer || waterHeight === 100) window.clearInterval(incTimer);
-    }, 0)
-
-    setTimeout(function (){
-      setDecTimer( setInterval(decFunc, 100) );
+  useEffect(()=>{
+    setTimeout(()=> {
+      setWaterHeight(he=> {
+        if ( step === 1 && he < 100) return he+1;
+        else if ( step === -1 && he > 0) return he-1;
+        else return he;
+      });
     }, 100)
-  }
+  }, [waterHeight])
 
   return (
     <div className="App">
@@ -43,8 +22,8 @@ function App() {
         </div>
       </div>
       <div className="buttons">
-        <button onClick={onIncrease}> increaseWaterLevel </button>
-        <button onClick={onDecrease}> decreaseWaterLevel </button>
+        <button onClick={()=>{setStep(1); if (waterHeight < 100) setWaterHeight(height=>height+1)}}> increaseWaterLevel </button>
+        <button onClick={()=>{setStep(-1); if (waterHeight > 0) setWaterHeight(height=>height-1)}}> decreaseWaterLevel </button>
       </div>
     </div>
   );
